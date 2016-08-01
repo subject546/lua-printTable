0d77de3629ffdab4f0449089aff5b0e14a489eb3
Subject546 <subject546M@gmail.com>
2016-08-01 20:56:24 +0200

    Update printTable.lua
    reduced the number of internal functions and optimized the "calllocal" loop

--- printTable.lua
+++ printTable.lua
@@ -1,15 +1,12 @@
 local function printTable(t,indent)
-  if indent == nil then indent = "" end
-  for key,value in pairs(t) do
-    if type(value) == "table" then
-      local indent2 = indent.."["..key.."]"
-      local function printTableFunc1(t,indent)
+      print(t,indent)
+          
+      local function printTableFunc(t,indent)
         if indent == nil then indent = "" end
         for key,value in pairs(t) do
           if type(value) == "table" then
             local indent2 = indent.."["..key.."]"
-            currentFunction = debug.getinfo(1,'n').name
-            return value,indent2,currentFunction
+            return value,indent2
           elseif type(value) == "string" or type(value) == "number" then
             local prefix = ""
             prefix = indent
@@ -26,58 +23,31 @@ local function printTable(t,indent)
           end
         end 
       end
-      local function printTableFunc2(t,indent)
-        if indent == nil then indent = "" end
-        for key,value in pairs(t) do
+      
+      local function callLocal(value,indent2)
+        if value ~= nil then
           if type(value) == "table" then
-            local indent2 = indent.."["..key.."]"
-            currentFunction = debug.getinfo(1,'n').name
-            return value,indent2,currentFunction
+            local currentValue,currentIndent = "",""
+            currentValue,currentIndent = printTableFunc(value,indent2)
+            callLocal(currentValue,currentIndent)
           elseif type(value) == "string" or type(value) == "number" then
             local prefix = ""
             prefix = indent
-            print(prefix.."["..key.."]".." -> ","","VALUE == "..value)
+            print(prefix.." -> ","","VALUE == "..value)
           elseif type(value) == "boolean" then
             local prefix = ""
             prefix = indent
-            print(prefix.."["..key.."]".." -> ","","BOOLEAN == "..tostring(value))
+            print(prefix.." -> ","","BOOLEAN == "..tostring(value))
           else
             local value = type(value)
             local prefix = ""
             if indent ~= nil then prefix = indent end
-            print(prefix.."["..key.."]".." -> ","","TYPE == "..value)
-          end
-        end 
-      end
-      local function callLocal(value,indent2,currentFunction) 
-        local currentValue,currentIndent = "",""
-        if currentFunction == false then
-          currentValue,currentIndent,currentFunction = printTableFunc1(value,indent2)
-          callLocal(currentValue,currentIndent,currentFunction)
-        elseif currentFunction == "printTableFunc1" then
-          currentValue,currentIndent,currentFunction = printTableFunc2(value,indent2)
-          callLocal(currentValue,currentIndent,currentFunction)
-        elseif currentFunction == "printTableFunc2" then
-          currentValue,currentIndent,currentFunction = printTableFunc1(value,indent2)
-          callLocal(currentValue,currentIndent,currentFunction)
+            print(prefix.." -> ","","TYPE == "..value)
+          end 
         end
       end
-      callLocal(value,indent2,false)
-    elseif type(value) == "string" or type(value) == "number" then
-      local prefix = ""
-      prefix = indent
-      print(prefix.."["..key.."]".." -> ","","VALUE == "..value)
-    elseif type(value) == "boolean" then
-      local prefix = ""
-      prefix = indent
-      print(prefix.."["..key.."]".." -> ","","BOOLEAN == "..tostring(value))
-    else
-      local value = type(value)
-      local prefix = ""
-      if indent ~= nil then prefix = indent end
-      print(prefix.."["..key.."]".." -> ","","TYPE == "..value)
-    end
-  end 
+      
+      callLocal(t,indent)
 end
 
 return printTable
